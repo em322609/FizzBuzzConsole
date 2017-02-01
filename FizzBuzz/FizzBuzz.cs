6949a1eff7e408a431beaa49561fea6489e58f60
em322609 <em322609@ohio.edu>
2017-02-01 10:53:24 -0500

    Initial FizzBuzz commit

---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    public class FizzBuzz
+    {
+        public string[] result;
+        public FizzBuzz()
+        {
+            result = new string[15];
+            SetValues();
+        }
+
+        public FizzBuzz(int min, int max)
+        {
+           
+            int size = max - min;
+            result = new string[size];
+            SetValues();
+        }
+
+        private void SetValues()
+        {
+            for (int i = 0; i < result.Length; i++)
+            {
+                if((i % 5 == 0) && (i % 3 == 0))
+                {
+                    result[i] = "fizzbuzz";
+                }
+                else if(i % 3 == 0)
+                {
+                    result[i] = "fizz";
+                }
+                else if(i % 5 == 0)
+                {
+                    result[i] = "buzz";
+                }
+                else
+                {
+                    result[i] = "" + i;
+                }
+
+            }
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.Write(result[i] + " ");
+            }
+        }
+    }
+}
---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using FizzBuzz;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Please enter a minimum value: ");
+            string sMin = Console.ReadLine();
+            int iMin = Convert.ToInt32(sMin);
+
+            Console.Write("Please enter a maximum value: ");
+            string sMax = Console.ReadLine();
+            int iMax = Convert.ToInt32(sMax);
+
+
+            if (iMin < iMax)
+            {
+                FizzBuzz fizz = new FizzBuzz(iMin, iMax);
+            }
+            else
+            {
+                FizzBuzz fizz = new FizzBuzz(iMax, iMin);
+            }
+    
+
+
+                Console.ReadLine();
+            
+        }
+    }
+}
